c775fc44b508a656e1ba19f46a1b33177d27eff2
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-03-12 19:39:18 -0400

    Fix

---
+++ src/AppWithRouterAccess.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, useHistory } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import Home from './Home';
+import Login from './Login';
+import Protected from './Protected';
+
+const AppWithRouterAccess = () => {
+    const history = useHistory();
+    const onAuthRequired = () => {
+        history.push('/login');
+    };
+
+    return (
+        <Security
+            issuer='/oauth2/default'
+            clientId=''
+            redirectUri={window.location.origin + '/implicit/callback'}
+            onAuthRequired={onAuthRequired}
+            pkce={true} >
+            <Route path='/' exact={true} component={Home} />
+            <SecureRoute path='/protected' component={Protected} />
+            <Route path='/login' render={() => <Login baseUrl='' />} />
+            <Route path='/implicit/callback' component={LoginCallback} />
+        </Security>
+    );
+};
+export default AppWithRouterAccess;
